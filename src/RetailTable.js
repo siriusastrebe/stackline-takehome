ad79e96c21a65d132826b455f795e421bdff12cb
sirius <siriusastrebe@gmail.com>
2024-01-09 13:19:25 -0800

    Table sorting.

--- src/RetailTable.js
+++ src/RetailTable.js
@@ -3,31 +3,52 @@ import "./RetailTable.css";
 import { currencyFormat } from './Helpers';
  
 function RetailTable(props) {
-  const [sort, setSort] = React.useState({column: 0, direction: 'asc'});
+  const [sort, setSort] = React.useState({column: 0, direction: 'desc'});
+
+  const clickHeader = React.useCallback((column) => {
+    let direction;
+    if (sort.column === column) {
+      direction = sort.direction === 'asc' ? 'desc' : 'asc' ;
+    } else {
+      direction = 'asc';
+    }
+    setSort({column: column, direction: direction});
+  }, [sort]);
+
+  const sortedSales = React.useMemo(() => {
+    const sales = [...props.product.sales];
+    const columns = ['weekEnding', 'retailSales', 'wholesaleSales', 'unitsSold', 'retailerMargin'];
+    sales.sort((a, b) => {
+      if (a[columns[sort.column]] < b[columns[sort.column]]) {
+        return sort.direction === 'asc' ? -1 : 1;
+      } else {
+        return sort.direction === 'asc' ? 1 : -1;
+      }
+    });
+    return sales;
+  }, [sort, props.product.sales])
 
   return (<div className="retail-table">
     <table className="retail-row">
       <tbody>
         <tr>
-          <th>
+          <th onClick={e => clickHeader(0)}>
             Week Ending {sortArrow(0, sort)}
           </th>
-          <th>
+          <th onClick={e => clickHeader(1)}>
             Retail Sales {sortArrow(1, sort)}
           </th>
-          <th>
+          <th onClick={e => clickHeader(2)}>
             Wholesale Sales {sortArrow(2, sort)}
           </th>
-
-          <th>
+          <th onClick={e => clickHeader(3)}>
             Units Sold {sortArrow(3, sort)}
           </th>
-
-          <th>
+          <th onClick={e => clickHeader(4)}>
             Retailer Margin {sortArrow(4, sort)}
           </th>
         </tr>
-        {props.product.sales.map(sale => (
+        {sortedSales.map(sale => (
           <tr>
             <td>{sale.weekEnding}</td>
             <td>{currencyFormat(sale.retailSales)}</td>
@@ -43,7 +64,7 @@ function RetailTable(props) {
 }
 function sortArrow(column, sort) {
   return <span className={`sortArrow ${sort.column === column ? 'active' : ''}`}>
-    {sort.column === column && sort.direction === 'asc' ? '▾' : '▴'}
+    {sort.column === column && sort.direction === 'asc' ? '▴' : '▾'}
   </span>
 }
 export default RetailTable;
