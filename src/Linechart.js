5bca4d048a8f5069681d3640c405ba73d2ed85f8
sirius <siriusastrebe@gmail.com>
2025-01-29 15:39:02 -0800

    Linechart now has yAxis values.

--- src/Linechart.js
+++ src/Linechart.js
@@ -7,7 +7,7 @@ function LineChartSVG(props) {
   const data = props.lineData;
   const chartConfiguration = props.chartConfiguration;
 
-  const margin = { top: 30, right: 30, bottom: 70, left: 60 };
+  const margin = { top: 30, right: 30, bottom: 70, left: 80 };
   const width = props.width - margin.left - margin.right;
   const height = props.height - margin.top - margin.bottom;
 
@@ -33,6 +33,18 @@ function LineChartSVG(props) {
     return m;
   }, [chartConfiguration.xDomain])
 
+  const yAxisNumbers = React.useMemo(() => {
+    const labels = [];
+    const start = chartConfiguration.yDomain[0];
+    const end = chartConfiguration.yDomain[1];
+    for (let i=0; i<5; i++) {
+      const t = i / 4;
+      labels.push(Math.round(start + (end - start) * t));
+    }
+    return labels;
+  }, [chartConfiguration.yDomain]);
+
+
   return (
     <svg style={{width: '100%', height: 400}} id="linechart">
       <g transform={`translate(${margin.left}, ${margin.top})`}>
@@ -46,6 +58,15 @@ function LineChartSVG(props) {
         {months.map((month, i) => {
           return (
             <text key={`month-text-${month.toISOString()}`} x={xScale(month)} y={height + margin.bottom/2} textAnchor="middle" opacity={.4} fontSize={'smaller'}>{monthAbbreviation(month)}</text>
+          )
+        })}
+      </g>
+      <g transform={`translate(10, ${margin.top})`}>
+        {yAxisNumbers.map((number, i) => {
+          return (
+            <text key={`number-${i}`} y={height - (i / 4 * height)} fontSize={`smaller`} opacity={0.7}>
+              ${commas(number)}
+            </text>
           )
         })}
       </g>
@@ -73,5 +94,14 @@ function Linechart(props) {
     <div className="chart-title">Retail Sales</div>
     <LineChartSVG width={width} height={400} chartConfiguration={chartConfiguration} lineData={data} />
   </div>)
+}
+function commas(number) {
+  let str = String(number);
+  for (let i=0; i<str.length - 1; i++) {
+    if (i > 0 && i % 4 === 2) {
+      str = str.slice(0, str.length - 1 - i) + ',' + str.slice(str.length - 1 - i);
+    }
+  }
+  return str;
 }
 export default Linechart;
